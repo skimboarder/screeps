265c06942b1a909424b4ba78c6e831a8889e059b
Connor DiMaio <skimborder@hotmail.com>
2016-07-09 13:26:34 -0400

    Update role_upgrader.js

--- role_upgrader.js
+++ role_upgrader.js
@@ -1,3 +1,6 @@
+var creepUtils = require('util.creepUtils');
+var structUtils = require('util.structureUtils');
+
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
@@ -5,28 +8,12 @@ var roleUpgrader = {
         
         if(creep.memory.upgrading && creep.carry.energy == 0) {
 	        creep.memory.upgrading = false;
-        }
-        if(!creep.memory.upgrading && (creep.carry.energy == creep.carryCapacity)) {
+        } else if(!creep.memory.upgrading && (creep.carry.energy == creep.carryCapacity)) {
             creep.memory.upgrading = true;
-        }
-        if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.flags.upgraderDestFlag);
-            }
-        }
-        if(!creep.memory.upgrading && creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_CONTAINER) &&
-                        structure.store[RESOURCE_ENERGY] > 0
-                }
-            });
-            
-            if(sources.length){
-                if(sources[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
-                }
-            }
+        } else if(creep.memory.upgrading) {
+            structUtils.upgradeCtrl(creep);
+        } else if(!creep.memory.upgrading && creep.carry.energy < creep.carryCapacity) {
+            creepUtils.getWorkerEnergy(creep);
         }
     }
     
