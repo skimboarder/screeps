9079c47c9620a3aac6646affc9db6aa4e0164dee
Connor DiMaio <skimborder@hotmail.com>
2016-07-09 13:25:56 -0400

    Update role_roadBuilder.js

--- role_roadBuilder.js
+++ role_roadBuilder.js
@@ -1,3 +1,6 @@
+var creepUtils = require('util.creepUtils');
+var structUtils = require('util.structureUtils');
+
 var roleRoadBuilder = {
 
     /** @param {Creep} creep **/
@@ -5,18 +8,9 @@ var roleRoadBuilder = {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
-	    }
-	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+	    } else if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
-	    }
-
-	    if(creep.memory.building) {
-	        var decaying = creep.room.find(FIND_STRUCTURES, {
-	            filter: (structure) => {
-	                return (structure.structureType == STRUCTURE_ROAD)
-	                    && structure.ticksToDecay < 25;
-	            }
-	        });
+	    } else if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
 	            filter: (structure) => {
 	                return (structure.structureType == STRUCTURE_ROAD)
@@ -24,32 +18,20 @@ var roleRoadBuilder = {
 	            }
 	        });
 	        if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-            }
-	        else if(decaying.length){
-	            if(creep.repair(decaying[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(decaying[0]);
-                }
-	        }
-
-	    }
-	    else {
-	        var sources = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    if((structure.structureType == STRUCTURE_CONTAINER) && (structure.store[RESOURCE_ENERGY] > 0)) {
-                        return structure;
-                    }
-
-                        
-                }
-            });
-            if(sources.length){
-                if(sources[0].transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
-                }
-            }
+                structUtils.buildStruct(creep, targets);
+            } else {
+                var decaying = creep.room.find(FIND_STRUCTURES, {
+	            filter: (structure) => {
+	                return (structure.structureType == STRUCTURE_ROAD)
+	                    && structure.hits < 5000;
+	                }
+	            });
+                if(decaying.length) {
+	                structUtils.repairStruct(creep, decaying);
+	            }
+	        } 
+	    } else {
+	        creepUtils.getWorkerEnergy(creep);
 	    }
 	}
 };
