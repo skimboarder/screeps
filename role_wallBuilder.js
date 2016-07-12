69a507f58df0d1e1a69e6494bc54b6a3f4676b5f
Connor DiMaio <skimborder@hotmail.com>
2016-07-12 14:15:55 -0400

    Update role_wallBuilder.js

--- role_wallBuilder.js
+++ role_wallBuilder.js
@@ -5,37 +5,41 @@ var roleWallBuilder = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        var closestHostile = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1000);
+        if(closestHostile) {
+            creep.attack(closestHostile[0]);
+        }
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
 	    }else if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
 	    }else if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
+	        var targets = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {
 	            filter: (structure) => {
 	                return (structure.structureType == STRUCTURE_WALL || structure.structureType == STRUCTURE_RAMPART)
 	                    && (structure.progress < structure.progressTotal);
 	            }
 	        });
 
-            var walls1 = creep.room.find(FIND_STRUCTURES, {
+            var walls1 = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (wall) => {
                     return (wall.structureType == STRUCTURE_RAMPART || wall.structureType == STRUCTURE_WALL)
                         && (wall.hits < 10000);
                 }
             });
 
-            var walls2 = creep.room.find(FIND_STRUCTURES, {
+            var walls2 = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (wall) => {
                     return (wall.structureType == STRUCTURE_RAMPART || wall.structureType == STRUCTURE_WALL)
                         && (wall.hits < 100000);
                 }
             });
-            if(targets.length) {
+            if(targets) {
                 structUtil.buildStruct(creep, targets);
-            } else if(walls1.length) {
+            } else if(walls1) {
                 structUtil.repairStruct(creep, walls1);
-            } else if(walls2.length) {
+            } else if(walls2) {
                 structUtil.repairStruct(creep, walls2);
             }
 	    }else {
@@ -43,27 +47,5 @@ var roleWallBuilder = {
 	    }
     }
 };
-
-function currentMax(creep) {
-    var max = 0;
-    
-    var walls = creep.room.find(FIND_STRUCTURES, {
-            filter: (wall) => {
-                return (wall.structureType == STRUCTURE_RAMPART || wall.structureType == STRUCTURE_WALL)
-                    && wall.hits > 1;
-            }
-    }); 
-    var w = 0;
-    if(walls.length){
-        while(w < walls.length) {
-            if(walls[w].hits > max) {
-                max = walls[w].hits;
-            }
-            w++;
-        }
-    }
-
-    return max + 500;
-}
 
 module.exports = roleWallBuilder;
