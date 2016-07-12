525f37203d6c7f555bd67aa24b234b5779558def
Connor DiMaio <skimborder@hotmail.com>
2016-07-12 14:13:06 -0400

    Update role_roadBuilder.js

--- role_roadBuilder.js
+++ role_roadBuilder.js
@@ -6,28 +6,41 @@ var roleRoadBuilder = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
+        var closestHostile = creep.pos.findInRange(FIND_HOSTILE_CREEPS, 1000);
+        if(closestHostile) {
+            creep.attack(closestHostile[0]);
+        }
+
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
 	    } else if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
 	    } else if(creep.memory.building) {
-	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
-	            filter: (structure) => {
-	                return (structure.structureType == STRUCTURE_ROAD)
-	                    && structure.progress < structure.progressTotal;
-	            }
-	        });
-	        if(targets.length) {
-                structUtils.buildStruct(creep, targets);
-            } else {
-                var decaying = creep.room.find(FIND_STRUCTURES, {
+	        if(creepUtils.shouldRebuild(creep)) {
+	            var decaying = creep.pos.findClosestByRange(FIND_STRUCTURES, {
 	            filter: (structure) => {
 	                return (structure.structureType == STRUCTURE_ROAD)
 	                    && structure.hits < 5000;
 	                }
 	            });
-                if(decaying.length) {
+	            
+                if(decaying) {
 	                structUtils.repairStruct(creep, decaying);
+	            } else {
+	                structUtils.upgradeCtrl(creep);
+	            }
+	        } else {
+	            var targets = creep.room.find(FIND_CONSTRUCTION_SITES, {
+	            filter: (structure) => {
+	                return (structure.structureType == STRUCTURE_ROAD)
+	                    && structure.progress < structure.progressTotal;
+	                }
+	            });
+	            _.sortBy(targets, function(t) { return (t.progressTotal - t.progress); });
+	            if(targets) {
+                    structUtils.buildStruct(creep, targets[0]);
+                } else {
+	                structUtils.upgradeCtrl(creep);
 	            }
 	        } 
 	    } else {
